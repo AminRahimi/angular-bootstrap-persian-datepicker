7754e07c5a39c4632afc853d97e161a6ffa85028
AminRahimi <a.rahimi1362@gmail.com>
2014-06-21 20:23:37 +0430

    fixed Demo Page

--- js/MainController.js
+++ js/MainController.js
@@ -1,4 +1,38 @@
 ﻿app.controller("MainController", function($scope){
-	$scope.fName = "amin";
-	$scope.dt=new Date(2014,5,20);
+
+	
+	$scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 6
+  };
+
+  $scope.initDate = new Date('2016-15-20');
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
   });

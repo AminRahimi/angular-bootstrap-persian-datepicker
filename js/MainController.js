7270fdb0f65f9e8aacad5c92e92028e35b05c0df
AminRahimi <a.rahimi1362@gmail.com>
2014-06-21 19:44:26 +0430

    first

---
+++ js/MainController.js
@@ -0,0 +1,4 @@
+﻿app.controller("MainController", function($scope){
+	$scope.fName = "amin";
+	$scope.dt=new Date(2014,5,20);
+  });
---
+++ js/app.js
@@ -0,0 +1,2 @@
+
+var app = angular.module('app',['ui.bootstrap']);
